6b35cd3f7c15f8a3497e890021efb305521c4247
anadevti <anacarolayne777@gmail.com>
2025-08-17 20:47:49 -0300

    feat: adding new types of infecteds

--- tlou-infected-api/src/Domain/Enums/InfectedStageSmartEnum.cs
+++ tlou-infected-api/src/Domain/Enums/InfectedStageSmartEnum.cs
@@ -4,6 +4,10 @@ public abstract class InfectedStageSmartEnum
 {
     public static readonly InfectedStageSmartEnum Runner = new RunnerStage();
     public static readonly InfectedStageSmartEnum Stalker = new StalkerStage();
+    public static readonly InfectedStageSmartEnum Clicker = new ClickerStage();
+    public static readonly InfectedStageSmartEnum Bloater = new BloaterStage();
+    public static readonly InfectedStageSmartEnum Shambler = new ShamblerStage();
+    public static readonly InfectedStageSmartEnum RatKing = new RatKingStage();
 
     public abstract string InfectionTime { get; }
     public abstract int InfectionStage { get; }
@@ -18,7 +22,30 @@ public abstract class InfectedStageSmartEnum
 
     private class StalkerStage : InfectedStageSmartEnum
     {
-        public override string InfectionTime => "7 Days";
+        public override string InfectionTime => "2 weeks and 1 month";
         public override int InfectionStage => 2;
-    }   
+    }
+    private class ClickerStage : InfectedStageSmartEnum
+    {
+        public override string InfectionTime => "1 Year";
+        public override int InfectionStage => 2;
+    } 
+    
+    private class BloaterStage : InfectedStageSmartEnum
+    {
+        public override string InfectionTime => "10 to 20 years";
+        public override int InfectionStage => 2;
+    }
+    
+    private class ShamblerStage : InfectedStageSmartEnum
+    {
+        public override string InfectionTime => "22 Years";
+        public override int InfectionStage => 2;
+    }
+    
+    private class RatKingStage : InfectedStageSmartEnum
+    {
+        public override string InfectionTime => "30 Years";
+        public override int InfectionStage => 2;
+    }
 }
---
+++ tlou-infected-api/src/Domain/Enums/InfectionStagingEnum.cs
@@ -0,0 +1,6 @@
+﻿namespace tlou_infected_api.Domain.Enums;
+
+public class InfectionStagingEnum
+{
+    
+}
