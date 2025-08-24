c75140c9bb333482c295b23a8735741d0087440c
anadevti <anacarolayne777@gmail.com>
2025-08-24 18:30:06 -0300

    feat: Faction Controller

---
+++ tlou-infected-api/src/Domain/DTO/CreateFactionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace tlou_infected_api.Domain.DTO;
+
+public class CreateFactionDto
+{
+    public string FactionType { get; set; }
+    public bool FactionStatus { get; set; }
+    public string Territory { get; set; }
+}
