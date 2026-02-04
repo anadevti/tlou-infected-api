a33099be90265a0eaed359435abe720bac679850
anadevti <anacarolayne777@gmail.com>
2026-02-03 21:29:28 -0300

    feat: add FluentValidation for SurvivorDto and configure validation in Program

--- tlou-infected-api/src/Domain/DTO/Survivor/CreateSurvivorDto.cs
+++ tlou-infected-api/src/Domain/DTO/Survivor/CreateSurvivorDto.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+using FluentValidation.Results;
 using tlou_infected_api.Domain.Enums;
 
 namespace tlou_infected_api.Domain.DTO.Survivor;
@@ -9,7 +11,43 @@ public class SurvivorDto
     public int Life { get; set; }
     public int Strength { get; set; }
     public int Agility { get; set; }
-    public required string MainWeapon { get; set; }
+    public string MainWeapon { get; set; }
     public int Stealth { get; set; }
     public SurvivorStatusEnum Status { get; set; }
+}
+
+public sealed class SurvivorValidator : AbstractValidator<SurvivorDto>
+{
+    public SurvivorValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("O Campo Name não pode ser vazio.");
+        
+        RuleFor(x => x.Life)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("O Campo Life não pode ser menor que zero.");
+
+        RuleFor(x => x.Strength)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("O Campo Strength não pode ser menor que zero.");
+
+        RuleFor(x => x.Agility)
+            .LessThanOrEqualTo(1)
+            .WithMessage("O Campo Agility não pode ser menor que zero.");
+        
+        RuleFor(x => x.MainWeapon)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O Campo MainWeapon não pode ser vazio.");
+    }
+    
+    public void ValidateSurvivor()
+    {
+        SurvivorDto survivor = new SurvivorDto();
+        var validator = new SurvivorValidator();
+
+        ValidationResult result = validator.Validate(survivor);
+    }
 }
